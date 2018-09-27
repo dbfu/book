74aa6df56a94b20506b9fec5b33cdaf942e07cc2
17364 <debao.fu@hand-china.com>
2018-09-27 23:54:03 +0800

    1

--- service.js
+++ service.js
@@ -6,7 +6,7 @@ var knex = require('knex')({
     password: '123456',
     database: 'books'
   },
-  debug: true, //指明是否开启debug模式，默认为true表示开启
+  debug: false, //指明是否开启debug模式，默认为true表示开启
   pool: { //指明数据库连接池的大小，默认为{min: 2, max: 10}
     min: 0,
     max: 7,
